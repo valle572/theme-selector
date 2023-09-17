5aa26b883ac41d5e7c6ec35f4ecbff4e7c96f4dd
Ruben Valle Ramirez <rubenvalle93@gmail.com>
2023-09-16 20:55:18 -0700

    Theme based on ID instead of Class
    Minor changes improve readability and are easier to understand. The theme is based on the ID of the body instead of the Class of the body

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-const themes = Array.from(document.querySelectorAll('button'), btn => btn.id);
-
-const changeTheme = selected => {
-   for (let theme of themes) document.body.classList.remove(theme);
-
-   localStorage.setItem('theme', selected);
-   document.body.classList.add(selected);
+const setTheme = theme => {
+   localStorage.setItem('theme', theme);
+   document.body.id = theme;
 }
-document.body.classList.add( localStorage.getItem('theme') || 'default' );
+document.body.id = localStorage.getItem('theme') || 'default';
